11b2bab3d65d0b6fd64609c9ec10e0992f55c185
Fabian Schmitthenner <development@schmitthenner.eu>
2025-03-02 14:02:35 +0100

    ,

--- app/src/main/java/eu/schmitthenner/transcribe/MainActivity.kt
+++ app/src/main/java/eu/schmitthenner/transcribe/MainActivity.kt
@@ -25,16 +25,28 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.net.toUri
+import androidx.compose.material.icons.Icons
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import eu.schmitthenner.transcribe.ui.theme.TransccribeTheme
@@ -47,6 +59,12 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.BufferedReader
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.ui.unit.dp
+import eu.schmitthenner.transcribe.ui.Main
+import eu.schmitthenner.transcribe.ui.RecordFilePicker
+import eu.schmitthenner.transcribe.ui.Settings
 import java.io.File
 import java.io.InputStreamReader
 import java.nio.ByteOrder
@@ -57,34 +75,15 @@ object Downloader {
 }
 
 class MainActivity : ComponentActivity() {
-    fun resamplePcm(input: ShortArray, inputRate: Int, outputRate: Int): ShortArray {
-        if (inputRate == outputRate) return input // No resampling needed
-
-        val ratio = outputRate.toFloat() / inputRate
-        val newSize = (input.size * ratio).toInt()
-        val resampled = ShortArray(newSize)
-
-        for (i in resampled.indices) {
-            val srcIndex = i / ratio
-            val srcIndexInt = srcIndex.toInt()
-            val srcIndexFrac = srcIndex - srcIndexInt
-
-            val sample1 = input.getOrElse(srcIndexInt) { 0 }
-            val sample2 = input.getOrElse(srcIndexInt + 1) { sample1 }
-
-            resampled[i] = (sample1 + (sample2 - sample1) * srcIndexFrac).toInt().toShort()
-        }
-
-        return resampled
-    }
-
-
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i("MainActivity", "MainActivity started")
         val model = ViewModelProvider(this).get(Model::class)
 
         val transcriber = Transcriber(this, WhisperCpp(this))
         val recorder = Recorder()
+
+        val recordFilePicker = RecordFilePicker(this@MainActivity, model, transcriber)
 
         val filePickerLogs = registerForActivityResult(ActivityResultContracts.CreateDocument("text/plain")) {
             logFile: Uri? ->
@@ -128,118 +127,6 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        val recordFilePicker = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
-            if (uri != null) {
-                model.setPlayFromFile(true)
-                lifecycleScope.launch(Dispatchers.IO) {
-                    val extractor = MediaExtractor()
-                    run {
-                        extractor.setDataSource(this@MainActivity, uri, null)
-                        extractor.selectTrack(0)
-
-                        val mediaFormat = extractor.getTrackFormat(0)
-                        val targetSampleRate = 16000  // Our desired output sample rate
-
-                        //val pcmEnc = mediaFormat.getInteger(MediaFormat.KEY_PCM_ENCODING)
-                        val inputSampleRate = mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE)
-                        val channels = mediaFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT)
-
-
-                        val codecName = MediaCodecList(MediaCodecList.REGULAR_CODECS).findDecoderForFormat(mediaFormat)
-                            ?: throw RuntimeException("No suitable decoder found")
-
-                        val codec = MediaCodec.createByCodecName(codecName)
-                        //mediaFormat.setInteger(MediaFormat.KEY_SAMPLE_RATE, 16000)
-
-                        codec.configure(mediaFormat, null, null, 0)
-
-
-                        codec.start()
-
-                        //Log.i("MainAct", "Output format: $pcmEnc $inputSampleRate $channels ${codec.outputFormat}")
-
-
-                        val bufferInfo = MediaCodec.BufferInfo()
-                        var isEOS = false
-                        var outputEOS = false
-
-                        while (!outputEOS) {
-                            // Feed input to the decoder
-                            if (!isEOS) {
-                                val inputBufferId = codec.dequeueInputBuffer(1000)
-                                if (inputBufferId >= 0) {
-                                    val inputBuffer = codec.getInputBuffer(inputBufferId) ?: continue
-                                    val sampleSize = extractor.readSampleData(inputBuffer, 0)
-
-                                    if (sampleSize < 0) {
-                                        codec.queueInputBuffer(
-                                            inputBufferId, 0, 0, 0,
-                                            MediaCodec.BUFFER_FLAG_END_OF_STREAM
-                                        )
-                                        isEOS = true
-                                    } else {
-                                        codec.queueInputBuffer(inputBufferId, 0, sampleSize, extractor.sampleTime, 0)
-                                        extractor.advance()
-                                    }
-                                }
-                            }
-
-                            // Retrieve output from the decoder
-                            var outputBufferId = codec.dequeueOutputBuffer(bufferInfo, 1000)
-                            while (outputBufferId >= 0) {
-                                val outputBuffer = codec.getOutputBuffer(outputBufferId)?.order(ByteOrder.nativeOrder())
-                                val shortBuffer = outputBuffer?.asShortBuffer()
-
-                                if (shortBuffer != null) {
-                                    val size = shortBuffer.remaining()
-
-
-                                    val rawPcmData = ShortArray(size / channels) { shortBuffer.get(channels*it) }
-
-                                    // Resample if needed
-                                    val resampledData = if (inputSampleRate != targetSampleRate) {
-                                        resamplePcm(rawPcmData, inputSampleRate, targetSampleRate)
-                                    } else {
-                                        rawPcmData
-                                    }
-
-                                    transcriber.pushData(resampledData, resampledData.size)
-                                }
-
-                                codec.releaseOutputBuffer(outputBufferId, false)
-
-                                if (bufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
-                                    outputEOS = true
-                                    break
-                                }
-
-                                outputBufferId = codec.dequeueOutputBuffer(bufferInfo, 1000)
-                            }
-                        }
-                        transcriber.recordingStopped()
-                        codec.stop()
-                        codec.release()
-                        extractor.release()
-                    }
-                }
-                model.setPlayFromFile(false)
-            }
-        }
-
-
-        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            isGranted: Boolean ->
-            if (isGranted) {
-                model.setHasRecordPermission()
-                model.toggleRecording(false)
-            }
-        }
-
-        val recordPermission: () -> Unit = {
-            requestPermissionLauncher.launch(
-                Manifest.permission.RECORD_AUDIO)
-        }
-
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
             model.setHasRecordPermission()
         }
@@ -261,6 +148,7 @@ class MainActivity : ComponentActivity() {
             recorder.run(self, self.lifecycleScope, transcriber, isRecording)
         }
 
+        @OptIn(ExperimentalMaterial3Api::class)
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
@@ -269,95 +157,60 @@ class MainActivity : ComponentActivity() {
             val transcription = transcriber.transcribedText.collectAsState();
             val transcriptionCount = transcriber.counter.collectAsState()
             val progress = transcriber.progress.collectAsState()
-            TransccribeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(Modifier.verticalScroll(rememberScrollState())) {
-                        Greeting(
-                            name = uiState.value.modelState.name,
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        Row {
-                            Button(onClick = {
-                                if (uiState.value.hasRecordPermission) model.toggleRecording(
-                                    uiState.value.isRecording
-                                ) else recordPermission()
-                            }, enabled = uiState.value.allowRecording()) {
-                                Text(if (uiState.value.isRecording) "Stop Recording" else "Record")
-                            }
-                            Button(
-                                enabled = uiState.value.allowRecording() && !uiState.value.isRecording,
+            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+            val scope = rememberCoroutineScope()
+            var selectedItem by remember { mutableStateOf("Main") }
+            val items = listOf("Main", "Settings")
+
+        TransccribeTheme {
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        items.forEach { item ->
+                            NavigationDrawerItem(
+                                label = { Text(item) },
+                                selected = item == selectedItem,
                                 onClick = {
-                                    recordFilePicker.launch(arrayOf("audio/*"))
-                                }
-                            ) {
-                                Text("Transcribe from file")
-                            }
-                        }
-
-                        Text(transcription.value)
-                        Text(transcriptionCount.value.toString())
-                        Text(progress.value.toString())
-
-                        val expanded = remember { mutableStateOf(false) }
-                        Text("Selected model: " + uiState.value.selectedModel?.name.orEmpty())
-                        Button(onClick = { expanded.value = true }) {
-                            Text("Switch model")
-                        }
-                        val selectedModel = uiState.value.selectedModel
-                        if (uiState.value.modelState == ModelState.DoesNotExist && selectedModel != null) {
-                            Button(onClick = {
-                                val downloadManager = getSystemService(DownloadManager::class.java)
-                                val request = DownloadManager.Request(selectedModel.downloadLocation())
-                                val enqueued = downloadManager.enqueue(request)
-                                Downloader.downloader.update { Triple(enqueued, selectedModel, model) }
-                                model.downloadStarted()
-                            }) { Text("Download") }
-                            Button(onClick = {
-                                filePicker2.launch(arrayOf("*/*"))
-                            }) { Text("Upload model manually")}
-                        }
-
-                        DropdownMenu(expanded = expanded.value, onDismissRequest = { expanded.value = false }) {
-                            for (i in SelectedModel.entries) {
-                                DropdownMenuItem(text = { Text(i.name)}, onClick = {
-                                    model.updateSelectedModel(i)
-                                    expanded.value = false
-                                })
-                            }
-                        }
-                        val transcriptionValue = transcription.value
-                        Button(onClick = {
-                            val clipboardManager = getSystemService(ClipboardManager::class.java)
-                            clipboardManager.setPrimaryClip(
-                                ClipData.newPlainText("Transcript", transcriptionValue)
+                                    scope.launch { drawerState.close() }
+                                    selectedItem = item
+                                },
+                                modifier = Modifier.padding(8.dp)
                             )
-                        }) {
-                            Text("Copy transcript")
-                        }
-                        Button(onClick = {
-                            filePickerLogs.launch("logs.txt")
-                        }) {
-                            Text("See logs")
                         }
                     }
                 }
+            ) {
+                Scaffold(topBar = {
+                    TopAppBar(navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+                        }
+
+                    }, title = {
+                        Text("Transccribe")
+                    })
+                }) { innerPadding ->
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        if (selectedItem == "Main") {
+                            Main(uiState, model, recordFilePicker)
+                        } else if(selectedItem == "Settings") {
+                            Settings()
+                            }
+                        }
+                    }
+                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TransccribeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/eu/schmitthenner/transcribe/ui/Main.kt
+++ app/src/main/java/eu/schmitthenner/transcribe/ui/Main.kt
@@ -1,4 +1,257 @@
 package eu.schmitthenner.transcribe.ui
 
-class Main {
+import android.Manifest
+import android.app.DownloadManager
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.media.MediaCodec
+import android.media.MediaCodecList
+import android.media.MediaExtractor
+import android.media.MediaFormat
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import eu.schmitthenner.transcribe.Downloader
+import eu.schmitthenner.transcribe.Model
+import eu.schmitthenner.transcribe.ModelState
+import eu.schmitthenner.transcribe.SelectedModel
+import eu.schmitthenner.transcribe.Transcriber
+import eu.schmitthenner.transcribe.UiState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.nio.ByteOrder
+
+
+class RecordFilePicker(context: ComponentActivity, model: Model, transcriber: Transcriber) {
+    private fun resamplePcm(input: ShortArray, inputRate: Int, outputRate: Int): ShortArray {
+        if (inputRate == outputRate) return input // No resampling needed
+
+        val ratio = outputRate.toFloat() / inputRate
+        val newSize = (input.size * ratio).toInt()
+        val resampled = ShortArray(newSize)
+
+        for (i in resampled.indices) {
+            val srcIndex = i / ratio
+            val srcIndexInt = srcIndex.toInt()
+            val srcIndexFrac = srcIndex - srcIndexInt
+
+            val sample1 = input.getOrElse(srcIndexInt) { 0 }
+            val sample2 = input.getOrElse(srcIndexInt + 1) { sample1 }
+
+            resampled[i] = (sample1 + (sample2 - sample1) * srcIndexFrac).toInt().toShort()
+        }
+
+        return resampled
+    }
+
+
+    val recordFilePicker = context.registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
+        if (uri != null) {
+            model.setPlayFromFile(true)
+            context.lifecycleScope.launch(Dispatchers.IO) {
+                val extractor = MediaExtractor()
+                run {
+                    extractor.setDataSource(context, uri, null)
+                    extractor.selectTrack(0)
+
+                    val mediaFormat = extractor.getTrackFormat(0)
+                    val targetSampleRate = 16000  // Our desired output sample rate
+
+                    //val pcmEnc = mediaFormat.getInteger(MediaFormat.KEY_PCM_ENCODING)
+                    val inputSampleRate = mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE)
+                    val channels = mediaFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT)
+
+
+                    val codecName = MediaCodecList(MediaCodecList.REGULAR_CODECS).findDecoderForFormat(mediaFormat)
+                        ?: throw RuntimeException("No suitable decoder found")
+
+                    val codec = MediaCodec.createByCodecName(codecName)
+                    //mediaFormat.setInteger(MediaFormat.KEY_SAMPLE_RATE, 16000)
+
+                    codec.configure(mediaFormat, null, null, 0)
+
+
+                    codec.start()
+
+                    //Log.i("MainAct", "Output format: $pcmEnc $inputSampleRate $channels ${codec.outputFormat}")
+
+
+                    val bufferInfo = MediaCodec.BufferInfo()
+                    var isEOS = false
+                    var outputEOS = false
+
+                    while (!outputEOS) {
+                        // Feed input to the decoder
+                        if (!isEOS) {
+                            val inputBufferId = codec.dequeueInputBuffer(1000)
+                            if (inputBufferId >= 0) {
+                                val inputBuffer = codec.getInputBuffer(inputBufferId) ?: continue
+                                val sampleSize = extractor.readSampleData(inputBuffer, 0)
+
+                                if (sampleSize < 0) {
+                                    codec.queueInputBuffer(
+                                        inputBufferId, 0, 0, 0,
+                                        MediaCodec.BUFFER_FLAG_END_OF_STREAM
+                                    )
+                                    isEOS = true
+                                } else {
+                                    codec.queueInputBuffer(inputBufferId, 0, sampleSize, extractor.sampleTime, 0)
+                                    extractor.advance()
+                                }
+                            }
+                        }
+
+                        // Retrieve output from the decoder
+                        var outputBufferId = codec.dequeueOutputBuffer(bufferInfo, 1000)
+                        while (outputBufferId >= 0) {
+                            val outputBuffer = codec.getOutputBuffer(outputBufferId)?.order(
+                                ByteOrder.nativeOrder())
+                            val shortBuffer = outputBuffer?.asShortBuffer()
+
+                            if (shortBuffer != null) {
+                                val size = shortBuffer.remaining()
+
+
+                                val rawPcmData = ShortArray(size / channels) { shortBuffer.get(channels*it) }
+
+                                // Resample if needed
+                                val resampledData = if (inputSampleRate != targetSampleRate) {
+                                    resamplePcm(rawPcmData, inputSampleRate, targetSampleRate)
+                                } else {
+                                    rawPcmData
+                                }
+
+                                transcriber.pushData(resampledData, resampledData.size)
+                            }
+
+                            codec.releaseOutputBuffer(outputBufferId, false)
+
+                            if (bufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                                outputEOS = true
+                                break
+                            }
+
+                            outputBufferId = codec.dequeueOutputBuffer(bufferInfo, 1000)
+                        }
+                    }
+                    transcriber.recordingStopped()
+                    codec.stop()
+                    codec.release()
+                    extractor.release()
+                }
+            }
+            model.setPlayFromFile(false)
+        }
+    }
+
+    fun launch() {
+        recordFilePicker.launch(arrayOf("audio/*"))
+    }
+
+    val requestPermissionLauncher = context.registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            isGranted: Boolean ->
+        if (isGranted) {
+            model.setHasRecordPermission()
+            model.toggleRecording(false)
+        }
+    }
+
+    fun requestRecordPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+    }
+
+}
+
+@Composable
+fun Main(uiState: State<UiState>, model: Model, recordFilePicker: RecordFilePicker) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            Text(
+                text = uiState.value.modelState.name,
+                modifier = Modifier.padding(innerPadding)
+            )
+            Row {
+                Button(onClick = {
+                    if (uiState.value.hasRecordPermission) model.toggleRecording(
+                        uiState.value.isRecording
+                    ) else recordFilePicker.requestRecordPermission()
+                }, enabled = uiState.value.allowRecording()) {
+                    Text(if (uiState.value.isRecording) "Stop Recording" else "Record")
+                }
+                Button(
+                    enabled = uiState.value.allowRecording() && !uiState.value.isRecording,
+                    onClick = {
+                        recordFilePicker.launch()
+                    }
+                ) {
+                    Text("Transcribe from file")
+                }
+            }
+
+            //Text(transcription.value)
+            //Text(transcriptionCount.value.toString())
+            //Text(progress.value.toString())
+
+            val expanded = remember { mutableStateOf(false) }
+            Text("Selected model: " + uiState.value.selectedModel?.name.orEmpty())
+            Button(onClick = { expanded.value = true }) {
+                Text("Switch model")
+            }
+            val selectedModel = uiState.value.selectedModel
+            if (uiState.value.modelState == ModelState.DoesNotExist && selectedModel != null) {
+                /*Button(onClick = {
+                    val downloadManager = getSystemService(DownloadManager::class.java)
+                    val request = DownloadManager.Request(selectedModel.downloadLocation())
+                    val enqueued = downloadManager.enqueue(request)
+                    Downloader.downloader.update { Triple(enqueued, selectedModel, model) }
+                    model.downloadStarted()
+                }) { Text("Download") }
+                Button(onClick = {
+                    filePicker2.launch(arrayOf("* / *"))
+                }) { Text("Upload model manually") } */
+            }
+
+            DropdownMenu(expanded = expanded.value, onDismissRequest = { expanded.value = false }) {
+                for (i in SelectedModel.entries) {
+                    DropdownMenuItem(text = { Text(i.name) }, onClick = {
+                        model.updateSelectedModel(i)
+                        expanded.value = false
+                    })
+                }
+            }
+            //val transcriptionValue = transcription.value
+            //Button(onClick = {
+            //    val clipboardManager = getSystemService(ClipboardManager::class.java)
+            //    clipboardManager.setPrimaryClip(
+            //        ClipData.newPlainText("Transcript", transcriptionValue)
+            //    )
+            //}) {
+            //    Text("Copy transcript")
+            //}
+            Button(onClick = {
+            //    filePickerLogs.launch("logs.txt")
+            }) {
+                Text("See logs")
+            }
+        }
+    }
 }
